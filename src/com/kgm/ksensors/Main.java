7f8c91e29fd2ce700b5837e8423280f07e85f9e9
Nocs13 <nocs13@gmail.com>
2014-07-02 20:18:56 +0400

    update code. add wifi, bluetooth,torch. move to dialog mode.

--- src/com/kgm/ksensors/Main.java
+++ src/com/kgm/ksensors/Main.java
@@ -1,30 +1,56 @@
 package com.kgm.ksensors;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 /**
  * Created by goga on 6/28/14.
  */
-public class Main extends Activity
-{
+public class Main extends Activity {
     kViewBase base = null;
 
-    public Main()
-    {
+    public Main() {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-      super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-      setVolumeControlStream(AudioManager.STREAM_MUSIC);
-      setVolumeControlStream(AudioManager.STREAM_VOICE_CALL);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        setVolumeControlStream(AudioManager.STREAM_VOICE_CALL);
 
-      base = new kViewBase(this);
-      setContentView(base);
+        base = new kViewBase(this);
+        setContentView(base);
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        //params.x = 10;
+        //params.height = getWindowManager().getDefaultDisplay().getHeight() - 10;
+        params.width = getWindowManager().getDefaultDisplay().getWidth() - 10;
+        //params.y = 10;
+
+        getWindow().setAttributes(params);
+    }
+
+    @Override
+    protected void onStop() {
+        Log.v("App:", "Stop");
+        super.onStop();
+
+        base.onStop();
+        finish();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.v("App:", "Stop");
+        super.onPause();
+
+        base.onStop();
+        finish();
     }
 }
--- src/com/kgm/ksensors/kViewBase.java
+++ src/com/kgm/ksensors/kViewBase.java
@@ -1,19 +1,20 @@
 package com.kgm.ksensors;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.media.AudioManager;
+import android.net.wifi.WifiManager;
 import android.os.RemoteException;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.widget.*;
 
 import java.lang.System;
 import java.util.logging.Logger;
@@ -23,12 +24,12 @@ import static android.provider.Settings.*;
 /**
  * Created by goga on 6/28/14.
  */
-public class kViewBase extends LinearLayout
-{
+public class kViewBase extends LinearLayout {
     public Context context = null;
 
-    public kViewBase(final Context context)
-    {
+    Camera camera = null;
+
+    public kViewBase(final Context context) {
         super(context);
 
         //setBackgroundColor(0xaa000000);
@@ -40,179 +41,341 @@ public class kViewBase extends LinearLayout
         addBrightness(context);
         addMusic(context);
         addVoice(context);
+        addWifi(context);
+        addBluetooth(context);
+        addTorch(context);
     }
 
-    void addBrightness(final Context context)
-    {
+    public void onStop() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    void addBrightness(final Context context) {
+        LinearLayout ll = new LinearLayout(context);
+
+        ll.setOrientation(HORIZONTAL);
+
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
-        lp.setMargins(20, 20, 20, 20);
+        lp.setMargins(20, 0, 20, 0);
+
+        addView(ll);
 
         TextView tv = new TextView(context);
         tv.setText("Brightness");
-        addView(tv);
+        tv.setMinimumWidth(100);
+        ll.addView(tv);
 
         SeekBar seeker = new SeekBar(context);
 
         seeker.setMax(255);
 
-        try
-        {
+        ll.addView(seeker);
+
+        seeker.setLayoutParams(lp);
+
+        try {
             seeker.setProgress(android.provider.Settings.System.getInt(context.getContentResolver(),
                     android.provider.Settings.System.SCREEN_BRIGHTNESS));
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
 
         }
 
-        addView(seeker);
-
-        seeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
-            public void onProgressChanged (SeekBar seekBar, int progress, boolean fromUser)
-            {
-                if(progress < 1)
+        seeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress < 1)
                     return;
 
-                try
-                {
+                try {
                     android.provider.Settings.System.putInt(context.getContentResolver(),
                             android.provider.Settings.System.SCREEN_BRIGHTNESS, progress);
-                    WindowManager.LayoutParams lp = ((Activity)context).getWindow().getAttributes();
-                    lp.screenBrightness = progress / (float)255;
-                    ((Activity)context).getWindow().setAttributes(lp);
-                }
-                catch(Exception e)
-                {
+                    WindowManager.LayoutParams lp = ((Activity) context).getWindow().getAttributes();
+                    lp.screenBrightness = progress / (float) 255;
+                    ((Activity) context).getWindow().setAttributes(lp);
+                } catch (Exception e) {
                     Log.v("Error", e.getMessage());
                 }
             }
-            public void onStartTrackingTouch(android.widget.SeekBar seekBar)
-            {
+
+            public void onStartTrackingTouch(android.widget.SeekBar seekBar) {
 
             }
 
-            public void onStopTrackingTouch(android.widget.SeekBar seekBar)
-            {
+            public void onStopTrackingTouch(android.widget.SeekBar seekBar) {
 
             }
         });
     }
 
-    void addMusic(final Context context)
-    {
+    void addMusic(final Context context) {
+        final AudioManager am = (AudioManager) ((Activity) context).getSystemService(Activity.AUDIO_SERVICE);
+
+        if (am == null)
+            return;
+
+        LinearLayout ll = new LinearLayout(context);
+
+        ll.setOrientation(HORIZONTAL);
+
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
-        lp.setMargins(20, 20, 20, 20);
+        lp.setMargins(20, 0, 20, 0);
+
+        addView(ll);
 
         TextView tv = new TextView(context);
         tv.setText("Music");
-        addView(tv);
+        tv.setMinimumWidth(100);
+        ll.addView(tv);
 
         SeekBar seeker = new SeekBar(context);
 
-        final AudioManager am = (AudioManager) ((Activity)context).getSystemService(Activity.AUDIO_SERVICE);
-
         seeker.setMax(am.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
 
-        try
-        {
+        seeker.setLayoutParams(lp);
+
+        ll.addView(seeker);
+
+        try {
             seeker.setProgress(android.provider.Settings.System.getInt(context.getContentResolver(),
                     Settings.System.VOLUME_MUSIC));
             seeker.setProgress(am.getStreamVolume(AudioManager.STREAM_MUSIC));
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
 
         }
 
-        addView(seeker);
-
-        seeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
-            public void onProgressChanged (SeekBar seekBar, int progress, boolean fromUser)
-            {
-                try
-                {
+        seeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                try {
                     android.provider.Settings.System.putInt(context.getContentResolver(),
                             Settings.System.VOLUME_MUSIC, progress);
                     am.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
-                }
-                catch(Exception e)
-                {
-                  Log.v("Error", e.getMessage());
+                } catch (Exception e) {
+                    Log.v("Error", e.getMessage());
                 }
             }
-            public void onStartTrackingTouch(android.widget.SeekBar seekBar)
-            {
+
+            public void onStartTrackingTouch(android.widget.SeekBar seekBar) {
 
             }
 
-            public void onStopTrackingTouch(android.widget.SeekBar seekBar)
-            {
+            public void onStopTrackingTouch(android.widget.SeekBar seekBar) {
 
             }
         });
     }
 
-    void addVoice(final Context context)
-    {
-      LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
-          LinearLayout.LayoutParams.WRAP_CONTENT);
-      lp.setMargins(20, 20, 20, 20);
+    void addVoice(final Context context) {
+        final AudioManager am = (AudioManager) ((Activity) context).getSystemService(Activity.AUDIO_SERVICE);
 
-      TextView tv = new TextView(context);
-      tv.setText("Call Voice");
-      addView(tv);
+        if (am == null)
+            return;
 
-      SeekBar seeker = new SeekBar(context);
+        LinearLayout ll = new LinearLayout(context);
 
-      final AudioManager am = (AudioManager) ((Activity)context).getSystemService(Activity.AUDIO_SERVICE);
+        ll.setOrientation(HORIZONTAL);
 
-      seeker.setMax(am.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL));
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(20, 0, 20, 0);
 
-      try
-      {
-        seeker.setProgress(android.provider.Settings.System.getInt(context.getContentResolver(),
-            Settings.System.VOLUME_MUSIC));
-        seeker.setProgress(am.getStreamVolume(AudioManager.STREAM_VOICE_CALL));
-      }
-      catch(Exception e)
-      {
+        addView(ll);
 
-      }
+        TextView tv = new TextView(context);
+        tv.setText("Call Voice");
+        tv.setMinimumWidth(100);
+        ll.addView(tv);
 
-      addView(seeker);
+        SeekBar seeker = new SeekBar(context);
 
-      seeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
-        public void onProgressChanged (SeekBar seekBar, int progress, boolean fromUser)
-        {
-          try
-          {
-            android.provider.Settings.System.putInt(context.getContentResolver(),
-                Settings.System.VOLUME_MUSIC, progress);
-            am.setStreamVolume(AudioManager.STREAM_VOICE_CALL, progress, 0);
-          }
-          catch(Exception e)
-          {
-            Log.v("Error", e.getMessage());
-          }
-        }
-        public void onStartTrackingTouch(android.widget.SeekBar seekBar)
-        {
+        seeker.setMax(am.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL));
+
+        seeker.setLayoutParams(lp);
+
+        ll.addView(seeker);
+
+        try {
+            seeker.setProgress(android.provider.Settings.System.getInt(context.getContentResolver(),
+                    Settings.System.VOLUME_MUSIC));
+            seeker.setProgress(am.getStreamVolume(AudioManager.STREAM_VOICE_CALL));
+        } catch (Exception e) {
 
         }
 
-        public void onStopTrackingTouch(android.widget.SeekBar seekBar)
-        {
+        seeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                try {
+                    android.provider.Settings.System.putInt(context.getContentResolver(),
+                            Settings.System.VOLUME_MUSIC, progress);
+                    am.setStreamVolume(AudioManager.STREAM_VOICE_CALL, progress, 0);
+                } catch (Exception e) {
+                    Log.v("Error", e.getMessage());
+                }
+            }
 
-        }
-      });
+            public void onStartTrackingTouch(android.widget.SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(android.widget.SeekBar seekBar) {
+
+            }
+        });
     }
 
-    public void draw_xxx(Canvas canvas)
-    {
-      canvas.saveLayerAlpha(null, 10, Canvas.ALL_SAVE_FLAG);
-      super.draw(canvas);
-      //canvas.restore();
+    void addWifi(final Context context) {
+        final WifiManager wm = (WifiManager) ((Activity) context).getSystemService(Activity.WIFI_SERVICE);
+
+        if (wm == null)
+            return;
+
+        LinearLayout ll = new LinearLayout(context);
+
+        ll.setOrientation(HORIZONTAL);
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(20, 0, 20, 0);
+
+        addView(ll);
+
+        TextView tv = new TextView(context);
+        tv.setText("Wifi");
+        tv.setMinimumWidth(100);
+        ll.addView(tv);
+
+        CheckBox checker = new CheckBox(context);
+
+        //checker.setLayoutParams(lp);
+
+        ll.addView(checker);
+
+        try {
+            checker.setChecked(wm.isWifiEnabled());
+        } catch (Exception e) {
+            Log.v("Error", e.getMessage());
+        }
+
+        checker.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                try {
+                    wm.setWifiEnabled(isChecked);
+                } catch (Exception e) {
+                    Log.v("Error", e.getMessage());
+                }
+            }
+        });
+    }
+
+    void addBluetooth(final Context context) {
+        final BluetoothAdapter ba = (BluetoothAdapter) BluetoothAdapter.getDefaultAdapter();
+
+        if (ba == null)
+            return;
+
+        LinearLayout ll = new LinearLayout(context);
+
+        ll.setOrientation(HORIZONTAL);
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
+                LayoutParams.WRAP_CONTENT);
+
+        lp.setMargins(20, 0, 20, 0);
+
+        addView(ll);
+
+        TextView tv = new TextView(context);
+        tv.setText("Bluetooth");
+        tv.setMinimumWidth(100);
+        ll.addView(tv);
+
+        CheckBox checker = new CheckBox(context);
+
+        //checker.setLayoutParams(lp);
+
+        ll.addView(checker);
+
+        try {
+            checker.setChecked(ba.isEnabled());
+        } catch (Exception e) {
+            Log.v("Error", e.getMessage());
+        }
+
+        checker.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                try {
+                    if (isChecked)
+                        ba.enable();
+                    else
+                        ba.disable();
+                } catch (Exception e) {
+                    Log.v("Error", e.getMessage());
+                }
+            }
+        });
+    }
+
+    void addTorch(final Context context) {
+        boolean hasTorch = context.getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (hasTorch == false)
+            return;
+
+        final Camera camera = Camera.open();
+
+        if (camera == null)
+            return;
+
+        this.camera = camera;
+
+        LinearLayout ll = new LinearLayout(context);
+
+        ll.setOrientation(HORIZONTAL);
+
+        addView(ll);
+
+
+        TextView tv = new TextView(context);
+        tv.setText("Flashlight");
+        tv.setMinimumWidth(100);
+        ll.addView(tv);
+
+        CheckBox checker = new CheckBox(context);
+
+        checker.setChecked(false);
+
+        ll.addView(checker);
+
+        final boolean isFlashOn = false;
+
+        checker.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                try {
+                    Camera.Parameters p = camera.getParameters();
+
+                    p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+
+                    if (isChecked) {
+                        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        camera.setParameters(p);
+                    } else {
+                        p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                        camera.setParameters(p);
+                    }
+                } catch (Exception e) {
+                    Log.v("Error", e.getMessage());
+                }
+            }
+        });
+    }
+
+    public void draw_xxx(Canvas canvas) {
+        canvas.saveLayerAlpha(null, 10, Canvas.ALL_SAVE_FLAG);
+        super.draw(canvas);
+        //canvas.restore();
     }
 }
